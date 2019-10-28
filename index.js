58db60c752888b38f71ff6826d6ed20d6d792629
frescocodes <hueydakotah@gmail.com>
2019-10-28 19:45:37 -0400

    feat: find() and insert() working

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // implement your API here
+const express = require('express')
+const db = require('./data/db.js')
+const server = express()
+const port = 8000
+
+
+server.use(express.json())
+
+// REQUEST HANDLERS
+// get request to '/' on localhost:8000
+server.get('/users', (req, res) => {
+    db.find()
+    .then(users => res.json(users))
+    .catch(error => {
+        console.log('error', error)
+        res.json({error: 'failed to get users'})
+    })
+})
+
+// POST
+server.post('/users', (req, res) => {
+    const userInfo = req.body
+
+    db.insert(userInfo)
+    .then(user => res.json(user))
+    .catch(error => {
+        console.log('error', error)
+        res.json({error: 'error creating user'})
+    })
+})
+
+server.listen(port, () => {
+    console.log(`server listening on port ${port}`)
+})
